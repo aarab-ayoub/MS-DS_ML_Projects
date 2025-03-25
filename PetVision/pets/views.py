71c401a556a1609564926a375b6d4fae096bd695
ayaarab <ayoubaarab571@gmail.com>
2025-03-25 16:09:41 -0400

    Update media file permissions, add media settings, and enhance training view with image loading

--- PetVision/pets/views.py
+++ PetVision/pets/views.py
@@ -1,3 +1,5 @@
+import os
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
@@ -7,11 +9,20 @@ def pets(request):
     return HttpResponse(template.render())
 
 def training(request):
-    template = loader.get_template("training.html")
-    return HttpResponse(template.render())
+    
+    
+    # cat_images = os.listdir(os.path.join(settings.MEDIA_ROOT, 'cats'))
+    # dog_images = os.listdir(os.path.join(settings.MEDIA_ROOT, 'dogs'))
+    cat_images = sorted(os.listdir(os.path.join(settings.MEDIA_ROOT, 'cats')))[:5]
+    dog_images = sorted(os.listdir(os.path.join(settings.MEDIA_ROOT, 'dogs')))[:5]
+    
+    context = {
+        'cat_images': cat_images,
+        'dog_images': dog_images,
+        'MEDIA_URL': settings.MEDIA_URL,
+    }
+    return render(request, 'training.html', context)
 
 def prediction(request):
     template = loader.get_template("prediction.html")
     return HttpResponse(template.render())
-
-
